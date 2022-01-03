95a59d3de4026426c70d5c4a8d11e26dca28bec5
Suleiman Mamman <suleimamman@gmail.com>
2022-01-03 15:36:22 +0100

    route created

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import config from "config"; 
 import connect from "./utils/connects";
 import logger from "./utils/logger"; 
+import routes from "./routes";
 
 const port = config.get<number>('port');
 const app = express();
  app.listen(port, () =>{
     logger.info(`App is running at in ${port}`);
     connect();
+    routes(app);
 });
